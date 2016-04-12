76a5505b95bdd5114b96d8a9cc8e8ccdcdba473f
Tomohisa Igarashi <tm.igarashi@gmail.com>
2016-04-12 12:58:03 +0900

    SWITCHYARD-2827-cxf-endpointconfigurer-example
    Added CxfEndpointConfigurer example on top of camel-cxf-binding quickstart

---
+++ quickstarts/camel-cxf-binding/src/main/java/org/switchyard/quickstarts/camel/cxf/MyExampleCxfEndpointConfigurer.java
@@ -0,0 +1,93 @@
+package org.switchyard.quickstarts.camel.cxf;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.inject.Named;
+
+import org.apache.camel.component.cxf.CxfEndpointConfigurer;
+import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.feature.Feature;
+import org.apache.cxf.frontend.AbstractWSDLBasedEndpointFactory;
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.interceptor.Interceptor;
+import org.apache.cxf.interceptor.InterceptorProvider;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.Phase;
+import org.apache.cxf.phase.PhaseInterceptor;
+import org.apache.log4j.Logger;
+
+@Named("myExampleCxfEndpointConfigurer")
+public class MyExampleCxfEndpointConfigurer implements CxfEndpointConfigurer {
+
+    private final Logger logger = Logger.getLogger(MyExampleCxfEndpointConfigurer.class);
+    
+    @Override
+    public void configure(AbstractWSDLBasedEndpointFactory factoryBean) {
+        factoryBean.getOutInterceptors().add(new MyExampleOutInterceptor());
+        for (Interceptor<? extends Message> interceptor : factoryBean.getOutInterceptors()) {
+            logger.info(String.format(">>>>> OutInterceptor [%s]", interceptor.getClass().getName()));
+        }
+        
+        factoryBean.getFeatures().add(new MyExampleFeature());
+        for (Feature feature : factoryBean.getFeatures()) {
+            logger.info(String.format(">>>>> Feature [%s]", feature.getClass().getName()));
+        }
+    }
+
+    class MyExampleOutInterceptor implements PhaseInterceptor<Message> {
+        @Override
+        public void handleMessage(Message message) throws Fault {
+        }
+        @Override
+        public void handleFault(Message message) {
+        }
+        @Override
+        public Set<String> getAfter() {
+            return new HashSet<String>();
+        }
+        @Override
+        public Set<String> getBefore() {
+            return new HashSet<String>();
+        }
+        @Override
+        public String getId() {
+            return MyExampleOutInterceptor.class.getName();
+        }
+        @Override
+        public String getPhase() {
+            return Phase.SETUP;
+        }
+        @Override
+        public Collection<PhaseInterceptor<? extends Message>> getAdditionalInterceptors() {
+            return null;
+        }
+    }
+    
+    class MyExampleFeature implements Feature {
+        @Override
+        public void initialize(Server server, Bus bus) {
+        }
+        @Override
+        public void initialize(Client client, Bus bus) {
+        }
+        @Override
+        public void initialize(InterceptorProvider interceptorProvider, Bus bus) {
+        }
+        @Override
+        public void initialize(Bus bus) {
+        }
+        
+    }
+    
+    @Override
+    public void configureClient(Client client) {
+    }
+    @Override
+    public void configureServer(Server server) {
+    }
+    
+}
